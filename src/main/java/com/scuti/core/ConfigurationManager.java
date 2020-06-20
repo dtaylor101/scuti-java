9a4ab95cba0fe1615415d6386448b820f1c8625c
HOUIS Mathis <59845782+kozennnn@users.noreply.github.com>
2020-06-20 18:11:48 +0200

    Configuration Manager

--- src/main/java/com/scuti/core/ConfigurationManager.java
+++ src/main/java/com/scuti/core/ConfigurationManager.java
@@ -4,12 +4,22 @@ import org.ini4j.*;
 import com.scuti.App;
 import java.io.File;
 
+
 public class ConfigurationManager {
+    public static String dbhost = "";
+    public static String dbport = "";
+    public static String dbname = "";
+    public static String dbuser = "";
+    public static String dbpass = "";
     public static void getConfiguration() {
         try{
             System.out.println(App.LOADING + "Loading configuration file...");
             Wini ini = new Wini(new File("config.ini"));
-            String testcc = ini.get("test", "cc");
+            dbhost = ini.get("database", "dbhost");
+            dbport = ini.get("database", "dbport");
+            dbname = ini.get("database", "dbname");
+            dbuser = ini.get("database", "dbuser");
+            dbpass = ini.get("database", "dbpass");
 
 
             System.out.println(App.SUCCESS + "Configuration manager -> OK!");
