e44b74761346a8a7f8c36da04cb41d91f6407e84
qtn06 <quentingarot06@gmail.com>
2020-06-20 03:10:51 +0200

    Update Room class

--- src/main/java/com/scuti/rooms/Room.java
+++ src/main/java/com/scuti/rooms/Room.java
@@ -1,4 +1,41 @@
 package com.scuti.rooms;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
 public class Room {
+    /** TODO: HashMap arrays for:
+     * -> users in this room
+     * -> items in this room
+     * -> bots in this room
+     * ...
+    **/
+    private final int id;
+    private final String name;
+    private final String description;
+    private final int ownerId;
+
+    public Room(ResultSet req) throws SQLException {
+        this.id = req.getInt("id");
+        this.name = req.getString("name");
+        this.description = req.getString("description");
+        this.ownerId = req.getInt("ownerid");
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getOwnerId() {
+        return this.ownerId;
+    }
 }
