c1b723134552562a2ce651dda1a2079f2e3db463
Tiig3r <leplusbeauswing@gmail.com>
2020-06-21 13:58:04 +0200

    Refactoring + add loadItems method

--- src/main/java/com/scuti/catalog/CatalogManager.java
+++ src/main/java/com/scuti/catalog/CatalogManager.java
@@ -11,23 +11,41 @@ import java.util.HashMap;
 
 public class CatalogManager {
     private static final HashMap<Integer, CatalogPage> pages = new HashMap<>();
-    private static final HashMap<Integer, CatalogCategorie> categories = new HashMap<>();
+    private static final HashMap<Integer, CatalogCategory> categories = new HashMap<>();
     private static final HashMap<Integer, CatalogItem> catalogItems = new HashMap<>();
 
     public HashMap<Integer, CatalogPage> getPages() {
         return pages;
     }
 
-    public static void load() {
-
+    public static void load() throws SQLException {
+        loadPages();
+        //loadItems();
     }
 
+    /*public static void loadItems() throws SQLException {
+        System.out.println(App.LOADING + "Loading catalog items...");
+        try(Connection connection = Database.getDB().getConnection()) {
+            try(Statement statement = connection.createStatement()) {
+                try(ResultSet req = statement.executeQuery("SELECT * FROM catalog_items")) {
+                    while(req.next()) {
+                        // TODO: create item class + database table corresponding!
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(App.ERROR + "Unable to load catalog items!");
+        }
+    }*/
+
     public static void loadPages() throws SQLException {
+        System.out.println(App.LOADING + "Loading catalog pages...");
         try(Connection connection = Database.getDB().getConnection()) {
             try(Statement statement = connection.createStatement()) {
                 try(ResultSet req = statement.executeQuery("SELECT * FROM catalog_pages")) {
                     while(req.next()) {
                         pages.put(req.getInt("id"), new CatalogPage(req));
+                        System.out.println(App.SUCCESS + "Catalog pages -> OK!");
                     }
                 }
             }
