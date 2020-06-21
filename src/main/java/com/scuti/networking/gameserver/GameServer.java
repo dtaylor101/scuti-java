d4b2ec7537187ca167bf8322978ab4d48656d3c5
HOUIS Mathis <59845782+kozennnn@users.noreply.github.com>
2020-06-21 21:07:36 +0200

    UI interaction with WebSockets

--- src/main/java/com/scuti/networking/gameserver/GameServer.java
+++ src/main/java/com/scuti/networking/gameserver/GameServer.java
@@ -5,6 +5,8 @@ import com.scuti.networking.Server;
 import com.scuti.networking.*;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
+import org.json.JSONObject;
+
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.*;
@@ -25,5 +27,30 @@ public class GameServer {
         System.out.println("Somebody left :(");
     }
 
+    @OnWebSocketMessage
+    public void onMessage(Session user, String message) {
+        if(message.equals("openCatalog")) {
+            System.out.println(message);
+            try {
+                user.getRemote().sendString(String.valueOf(new JSONObject()
+                        .put("canOpenCatalog", "true")
+                ));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if(message.equals("openNavigator")) {
+            System.out.println(message);
+            try {
+                user.getRemote().sendString(String.valueOf(new JSONObject()
+                        .put("canOpenNavigator", "true")
+                ));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Get:" + message);
+        }
+
+    }
 
 }
